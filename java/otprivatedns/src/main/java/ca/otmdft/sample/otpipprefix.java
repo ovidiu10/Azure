0bbd6cca6591d5bb42227de1d3c763a14fe31436
Ovidiu10 <timpanariu_ovidiu@hotmail.com>
2020-03-06 09:06:10 -0500

    ip

---
+++ java/otprivatedns/src/main/java/ca/otmdft/sample/otpipprefix.java
@@ -0,0 +1,56 @@
+package ca.otmdft.sample;
+
+import com.microsoft.azure.arm.resources.Region;
+import com.microsoft.azure.credentials.ApplicationTokenCredentials;
+import com.microsoft.azure.management.Azure;
+import com.microsoft.rest.LogLevel;
+
+import com.microsoft.azure.management.network.*;
+import com.microsoft.azure.management.resources.implementation.ResourceManager;
+import com.microsoft.azure.management.resources.Provider;
+import com.microsoft.azure.management.resources.fluentcore.arm.ResourceUtils;
+
+import java.io.File;
+
+public class otpipprefix {
+    public static void main(String[] args) {
+        System.out.println("Public Prefix IP");
+        try {
+            final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
+            Azure azure = Azure.configure().withLogLevel(LogLevel.NONE).authenticate(credFile)
+                    .withDefaultSubscription();
+            String id1 = "/subscriptions/42581afb-e04b-43d7-91a7-8faca6703fe7/resourceGroups/rg-lb1/providers/Microsoft.Network/publicIPAddresses/ips1";
+            
+            ResourceUtils.defaultApiVersion(id1, provider)
+            ApplicationTokenCredentials cred = ApplicationTokenCredentials.fromFile(credFile);
+            PublicIPPrefix p1 = azure.publicIPPrefixes()
+                                    .define("ippool1")
+                                    .withRegion("eastus2")
+                                    .withExistingResourceGroup("group")
+                                    .withPrefixLength(8)
+                                    .withPublicIPAddressVersion(IPVersion.IPV4)
+                                    .create();
+            //PublicIPPrefix.DefinitionStages.
+            PublicIPAddress ip2 = azure.publicIPAddresses()
+                                    .define("ip2")
+                                    .withRegion("eastus2")
+                                    .withExistingResourceGroup("group")
+                                    .withStaticIP()
+                                    .withSku(PublicIPSkuType.STANDARD)
+                                    .create();
+            
+            PublicIPAddress.DefinitionStages.WithCreate ip1 = azure.publicIPAddresses()
+                .define("ip1")
+                .withRegion("eastus2")
+                .withExistingResourceGroup("group")
+                .withStaticIP()
+                .withIdleTimeoutInMinutes(4);
+            ip1.withSku(PublicIPSkuType.STANDARD);
+            
+            ip1.create();
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println( "Done" );
+}
