39d4b24934a4cb403a1173b0ea60bd97985cfd2c
ovidiu10 <timpanariu_ovidiu@hotmail.com>
2021-07-04 18:40:57 -0400

    Java1

--- java/otsnfFD/src/main/java/io/otmsft2/samplecrp/otvmforcedelete.java
+++ java/otsnfFD/src/main/java/io/otmsft2/samplecrp/otvmforcedelete.java
@@ -7,8 +7,16 @@ import com.microsoft.azure.management.network.NetworkInterface;
 import com.microsoft.azure.management.resources.GenericResource;
 import com.microsoft.rest.LogLevel;
 
+import org.joda.time.DateTime;
+import org.joda.time.Seconds;
+
 import java.io.File;
+import java.time.LocalDateTime;
 import java.util.Date;
+
+import javax.swing.text.StyledEditorKit;
+
+import static java.time.temporal.ChronoUnit.SECONDS;
 
 public class otvmforcedelete
 {
@@ -16,18 +24,29 @@ public class otvmforcedelete
     {
         //!!!Assumption already exit 3 Network Interface "nicvmNameX" where X = 1:3 
         //!!!Interface 1 - VIP Basic; Interface 2 - VIP Standard; Interface 3 - No VIP
-        final String rgName = "rg-java2"; // Resource GroupName - should be exist 
-        final String region = "centraluseuap"; // Region in Azure; 
-        System.out.println("Start Testing " + new Date());
+        //!!!nicNvmName - all no VIP 
+        String rgName = "rg-java3"; // Resource GroupName - should be exist 
+        String region = "eastus2euap"; // Region in Azure;
+        final String reg = "east"; 
+        final String nicName = "nicvmName";
         try
         {
             final File credFile = new File(System.getenv("AZURE_AUTH_LOCATION"));
             final Azure azure = Azure.configure().withLogLevel(LogLevel.NONE).authenticate(credFile)
-                    .withDefaultSubscription();            
+                    .withDefaultSubscription();                     
+            if (reg == "east")
+            {
+                rgName = "rg-java3";
+                region = "eastus2euap";
+            } else {            
+                rgName = "rg-java2";
+                region = "centraluseuap";
+            }                  
+            System.out.println("Start Testing " + region + " " + new Date());
             for (int i = 1; i < 4; i++) {
                 String vmName = "vmName" + i;
                 System.out.println("VM Name: " + vmName);                
-                NetworkInterface nic = azure.networkInterfaces().getByResourceGroup(rgName, "nicvmName" + i);                                         
+                NetworkInterface nic = azure.networkInterfaces().getByResourceGroup(rgName, nicName + i);                                         
                 VirtualMachine vmLinux = azure.virtualMachines().define(vmName)
                                             .withRegion(region).withExistingResourceGroup(rgName)
                                             .withExistingPrimaryNetworkInterface(nic)
@@ -39,6 +58,7 @@ public class otvmforcedelete
                 System.out.println(vmLinux.id());                         
                 String osDiskid = vmLinux.osDiskId();
                 VirtualMachine vmLinux_d = azure.virtualMachines().getByResourceGroup(rgName, vmName);
+                LocalDateTime dataStart = LocalDateTime.now();
                 System.out.println("Timer task started at: "+new Date());
                 GenericResource genericResource = azure.genericResources().getById(vmLinux_d.id());
                 azure.genericResources().delete(
@@ -50,7 +70,10 @@ public class otvmforcedelete
                     "2020-06-01",
                     true
                 );
-                System.out.println("Timer task finished at: "+new Date());                            
+                LocalDateTime dataEnd = LocalDateTime.now();
+                long diff = SECONDS.between(dataStart, dataEnd);
+                System.out.println("Timer task finished at: "+new Date());
+                System.out.println(diff + "s");                            
                 System.out.println("\n");
                 System.out.println("Timer task started Delete OS at: "+new Date());
                 GenericResource genericResource_os = azure.genericResources().getById(osDiskid);
