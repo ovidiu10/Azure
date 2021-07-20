9e512e813f70ac9ead56eb644c4784f0002db122
ovidiu10 <timpanariu_ovidiu@hotmail.com>
2021-07-20 11:25:41 -0400

    graph and storage

---
+++ java/otprivatedns/src/main/java/ca/otmdft/sample/otstoragesdk.java
@@ -0,0 +1,18 @@
+package ca.otmdft.sample;
+
+import java.io.File;
+
+public class otstoragesdk 
+{
+    public static void main( String[] args )
+    {
+        System.out.println("Storage SDK");
+        try {
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        System.out.println( "Done" );
+    }
+}
