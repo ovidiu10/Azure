4ad754faf171f36b40ce1efbd0810d83ed3d53aa
ovidiu10 <timpanariu_ovidiu@hotmail.com>
2023-11-07 11:59:05 -0500

    Update Nov 2023.1

--- java/otstorage/src/main/java/ca/otmdft/sample/ostoragetest1.java
+++ java/otstorage/src/main/java/ca/otmdft/sample/ostoragetest1.java
@@ -1,14 +1,17 @@
 package ca.otmdft.sample;
 
+import java.io.UncheckedIOException;
 import java.util.Locale;
 
 import com.azure.core.credential.TokenCredential;
 import com.azure.core.http.policy.HttpLogDetailLevel;
+import com.azure.core.http.policy.UserAgentPolicy;
 import com.azure.core.http.rest.Response;
 import com.azure.core.management.AzureEnvironment;
 import com.azure.core.management.Region;
 import com.azure.core.management.profile.AzureProfile;
 import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.identity.DefaultAzureCredential;
 import com.azure.resourcemanager.AzureResourceManager;
 import com.azure.resourcemanager.compute.models.KnownLinuxVirtualMachineImage;
 import com.azure.resourcemanager.compute.models.VirtualMachine;
@@ -20,6 +23,7 @@ import com.azure.storage.blob.BlobAsyncClient;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobClientBuilder;
 import com.azure.storage.blob.BlobContainerAsyncClient;
+import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.BlobServiceAsyncClient;
 import com.azure.storage.blob.BlobServiceClient;
 import com.azure.storage.blob.BlobServiceClientBuilder;
@@ -27,68 +31,34 @@ import com.azure.storage.blob.options.BlobBeginCopyOptions;
 
 public class ostoragetest1 {
     public static void main(final String[] args) {
-        System.out.println("Azure SDK Storage");
-        //System.out.println("ostoragetest1");
-        String accountName = "ostoragetest11";
+        System.out.println("Azure SDK Storage");        
+        String accountName = "";
         String rgName = "rg-java";
         String sContainerName = "container5";
-        String sblobName = "AzureIaaSWS.zip"; 
-        //String rgName = "rg-bkparchive1"; // Resource GroupName - should be exist 
-        //String vnetName = "vnet-wcus";
-        //String subnetName = "SB_192.168.229.0_25";
+        String sblobName = ""; 
+        String filePathName = "";
         String endpoint = String.format(Locale.ROOT, "https://%s.blob.core.windows.net", accountName);
-        final AzureProfile profile = new AzureProfile(AzureEnvironment.AZURE);
-        final TokenCredential credential = new DefaultAzureCredentialBuilder()
-            .authorityHost(profile.getEnvironment().getActiveDirectoryEndpoint())
-            .build();             
-        AzureResourceManager azureResourceManager = AzureResourceManager
-            .configure()
-            .withLogLevel(HttpLogDetailLevel.BODY_AND_HEADERS)
-            .authenticate(credential, profile)
-            .withSubscription("");
-        String encryptionScope = "OTMS1";
-        /*
-        if (azureResourceManager.storageAccounts().checkNameAvailability(accountName).isAvailable()) {                               
-            StorageAccount storageAccount = azureResourceManager.storageAccounts().define(accountName)
-                .withRegion(Region.US_WEST)
-                .withExistingResourceGroup(rgName)
-                .withGeneralPurposeAccountKindV2()
-                .create();
-        }
-        */
-        /*
-        BlobServiceAsyncClient blobServiceClient = new BlobServiceClientBuilder()
-            .endpoint(endpoint)
-            .credential(new DefaultAzureCredentialBuilder().build())            
-            .encryptionScope(encryptionScope)
-            .buildAsyncClient();
-        //System.out.println(storagClient);
-        BlobContainerAsyncClient blobContainerAsyncClient = blobServiceClient.getBlobContainerAsyncClient(sContainerName);
-        BlobAsyncClient blobAsyncClient = blobContainerAsyncClient.getBlobAsyncClient(sblobName);
-        BlobBeginCopyOptions beginCopyOptions = new BlobBeginCopyOptions(sourceUrl);
-        blobAsyncClient.beginCopy(beginCopyOptions);
-        */
-        /*
-        Network virtualNetwork1 = azureResourceManager.networks().getByResourceGroup(rgName, vnetName);
-        VirtualMachine vm2 = azureResourceManager.virtualMachines().define("otvm61wcus")
-                .withRegion(Region.US_WEST_CENTRAL.name())
-                .withExistingResourceGroup(rgName)
-                .withExistingPrimaryNetwork(virtualNetwork1)
-                .withSubnet(subnetName)
-                .withPrimaryPrivateIPAddressDynamic()
-                .withoutPrimaryPublicIPAddress()
-                .withPopularLinuxImage(KnownLinuxVirtualMachineImage.UBUNTU_SERVER_18_04_LTS)
-                .withRootUsername("admin22")
-                .withRootPassword("test#123469")
-                //.withBootDiagnostics()                
-                .withBootDiagnosticsOnManagedStorageAccount()
-                .withSize(VirtualMachineSizeTypes.STANDARD_B1S)
-                .create();
-        */
-        /*
-        VirtualMachine vm1 = azureResourceManager.virtualMachines().getByResourceGroup(rgName, "otvm6wcus");
-        vm1.update().withTag("adobe:ms:manage-elb-links:detached-from:", "attach").apply();
-        */
+        DefaultAzureCredential credential = new DefaultAzureCredentialBuilder()
+                    .tenantId("XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX")
+                    .build();                     
+        UserAgentPolicy userAgentPolicy = new UserAgentPolicy("ostoragetest1/0.0.1 azsdk-java-storage-blob/12.15.0 (Java 11; Windows_NT 10.0.22631.2506");
+        BlobServiceClient blobServiceClient = new BlobServiceClientBuilder()
+                .endpoint(endpoint)
+                .credential(credential)
+                .addPolicy(userAgentPolicy)
+                .buildClient();
+        BlobContainerClient blobContainerClient = blobServiceClient.getBlobContainerClient(sContainerName);        
+        uploadBlobFromFile(blobContainerClient, filePathName, sblobName);
         System.out.println("Done");
     }
+
+    public static void uploadBlobFromFile(BlobContainerClient blobContainerClient, String filePathName, String sblobName) 
+    {
+        BlobClient blobClient = blobContainerClient.getBlobClient(sblobName);
+        try {
+            blobClient.uploadFromFile(filePathName);
+        } catch (UncheckedIOException ex) {
+            System.err.printf("Failed to upload from file: %s%n", ex.getMessage());
+        }
+}
 }
