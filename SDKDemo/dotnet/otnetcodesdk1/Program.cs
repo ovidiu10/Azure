32dace914e6f680c1b03047b76d56c88e64cd6e1
ovidiu10 <timpanariu_ovidiu@hotmail.com>
2025-01-21 20:31:11 -0500

    clean Jan 2025

--- SDKDemo/dotnet/otnetcodesdk1/Program.cs
+++ SDKDemo/dotnet/otnetcodesdk1/Program.cs
@@ -5,13 +5,19 @@ using Azure.ResourceManager;
 using Azure.ResourceManager.Resources;
 
 // See https://aka.ms/new-console-template for more information
+string tenantId = "";
+string subscriptionId = "";
 Console.WriteLine("SDK Testing!");
 ArmClient armClient = new ArmClient(new DefaultAzureCredential(
-    new DefaultAzureCredentialOptions { TenantId = "3daa70ca-2390-4c3a-8d40-86f93d5d5b10" }
+    new DefaultAzureCredentialOptions { TenantId = tenantId }
 ));
 SubscriptionCollection subscriptions = armClient.GetSubscriptions();
-SubscriptionResource subscription = subscriptions.Get("8451578e-4ec2-47cb-aa28-45d9209f3a9c");
+SubscriptionResource subscription = subscriptions.Get(subscriptionId);
 ResourceGroupCollection rgCollection = subscription.GetResourceGroups();
 Console.WriteLine("Subscription ID: " + subscription.Data.SubscriptionId);
 Console.WriteLine("Getting resource groups...");
 Console.WriteLine($"Found {rgCollection.Count()} resource groups in the subscription");
+foreach (ResourceGroupResource rg in rgCollection)
+{
+    Console.WriteLine($"Resource Group: {rg.Data.Name}");
+}   
